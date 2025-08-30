25ad35da11c402b20092602c9ada42988e16fbe4
thiagolages <thiagolagesrocha@gmail.com>
2025-08-30 15:53:46 -0300

    Add initial TP1 code
    Add initial TP1 code

--- src/Assingment.py
+++ src/Assingment.py
@@ -8,7 +8,7 @@ from CoppeliaSimAPI import CoppeliaSimAPI  # noqa: F401, E402
 class Assignment:
     def __init__(self, sleep=True, auto_start=True, verbose=0):
         if sleep:
-            print("Waiting for CoppeliaSim to start...")
+            print("Waiting for CoppeliaSim to open...")
             time.sleep(3)
         self.port = os.environ.get("COPPELIA_ZMQ_PORT", 23000)
         try:
@@ -18,6 +18,7 @@ class Assignment:
             self.sim = self.simAPI.sim
             if auto_start:
                 self.simAPI.start()
+                time.sleep(1)  # wait for the simulation to start
         except Exception as e:
             print("CoppeliaSim not reachable (this is fine in CI):", e)
             exit(1)
--- src/tp1.py
+++ src/tp1.py
@@ -1,17 +1,15 @@
-import matplotlib
-
 from Assingment import Assignment
 from utils import (
-    get_objects_handles,
-    get_objects_poses,
+    build_object_dict,
+    get_tf,
+    handle_sim_start,
+    plot_robot_to_object_tfs,
 )
-
-matplotlib.use("TkAgg")  # Use TkAgg backend for interactive display
 
 
 class TP1(Assignment):
     def __init__(self):
-        super().__init__(sleep=False)
+        super().__init__(sleep=False, auto_start=False)
         self._setup()
 
     def _setup(self):
@@ -21,11 +19,37 @@ class TP1(Assignment):
             "DefaultLights",
             "Floor",
         ]
-        self.objects = get_objects_handles(self.sim, self.excluded_objects)
-        self.objects = get_objects_poses(self.sim, self.objects, verbose=True)
+        self.robot_name = "hexapod"
+        self.objects = build_object_dict(self.sim, self.excluded_objects)
+        self.robot_handle = self.sim.getObject("/" + self.robot_name)
+
+    def get_robot_tfs(self):
+        self.T_w_robot = get_tf(
+            self.sim, self.robot_handle, verbose=True
+        )  # 4 x 4 matrix
+        self.T_robot_w = get_tf(
+            self.sim, self.robot_handle, inv=True, verbose=True
+        )  # 4 x 4 matrix
+
+    def get_robot_to_object_tfs(self):
+        self.robot_to_obj_tfs = {}
+        for name, obj in self.objects.items():
+            handle = obj["handle"]
+            T_w_obj = get_tf(self.sim, handle, verbose=False)  # 4 x 4 matrix
+            # T_w_obj = T_w_robot @ T_robot_obj
+            # -> left multiply by (T_w_robot)^-1, or T_robot_w
+            # (T_w_robot)^-1 @ T_w_obj = T_robot_obj
+            # T_robot_obj = T_robot_w @ T_w_obj
+            T_robot_obj = self.T_robot_w @ T_w_obj
+            self.robot_to_obj_tfs[name] = T_robot_obj
+            print(f"Transform from robot to {name}: {T_robot_obj}")
+            print("-" * 40)
 
     def run(self):
-        pass
+        handle_sim_start(self.sim)
+        self.get_robot_tfs()
+        self.get_robot_to_object_tfs()
+        plot_robot_to_object_tfs(self.robot_name, self.robot_to_obj_tfs)
 
 
 if __name__ == "__main__":
